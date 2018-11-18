df57fa49173127ef54f9f6883463386e62b3d3e9
DESKTOP-6JOJD7C\Kuba <brodaczz1@gmail.com>
2018-11-18 15:23:12 +0100

    enemy spawning and movement, some terrain

---
+++ Assets/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.Rotate(new Vector3(0.0f, 180.0f, 0.0f));
+       // this.GetComponent<Rigidbody>().AddForce(Vector3.forward*4f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.back;
+	}
+}
